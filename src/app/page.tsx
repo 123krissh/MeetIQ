80d6fcdb66ff064535fbfd9ed9d5a8b82fad6858
Vikram <vikramranjan71122@gmail.com>
2025-08-16 20:46:27 +0530

    Rearrange content in Home component for better readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
     <>
       <div className="text-4xl font-bold text-green-500">Hello World</div>
       <Button variant="destructive">save</Button>
-      <p className="text 2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati unde eius, nesciunt cum laborum tenetur corrupti fugit nam. Eveniet dicta architecto quo voluptatum corporis. Cumque rerum sunt neque consequuntur alias.</p>
+      <p>Vikram Ranjan</p>
+      <p className="text 2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati unde
+        eius, nesciunt cum laborum tenetur corrupti fugit nam. Eveniet dicta
+        architecto quo voluptatum corporis. Cumque rerum sunt neque consequuntur
+        alias.
+      </p>
     </>
   );
 }
