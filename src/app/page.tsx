07220ee3161859adf7bf8f2cbebb34fe52cf715a
Vikram <vikramranjan71122@gmail.com>
2025-08-16 22:57:19 +0530

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,62 @@
+"use client";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client"; //import the auth client
+
 import { Button } from "@/components/ui/button";
 export default function Home() {
+  const { data: session } = authClient.useSession();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        password,
+        name,
+      },
+      {
+        onSuccess: () => {
+          window.alert("Success");
+        },
+        onError: (ctx) => {
+          window.alert("Something went wrong");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}></Button>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <div className="text-4xl font-bold text-green-500">Hello World</div>
-      <Button variant="destructive">save</Button>
-      <p>Vikram Ranjan</p>
-      <p className="text 2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati unde
-        eius, nesciunt cum laborum tenetur corrupti fugit nam. Eveniet dicta
-        architecto quo voluptatum corporis. Cumque rerum sunt neque consequuntur
-        alias.
-      </p>
-    </>
+    <div>
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></Input>
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      ></Input>
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></Input>
+      <Button onClick={onSubmit}>Create User</Button>
+    </div>
   );
 }
