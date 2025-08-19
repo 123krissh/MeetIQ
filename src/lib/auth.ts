2bdb653e9e975337c86f7e6308ce5c188cc52ca4
Vikram <vikramranjan71122@gmail.com>
2025-08-19 23:03:20 +0530

    sign-up

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,3 @@
-import { Schema } from './../../node_modules/better-auth/node_modules/zod/src/v4/core/json-schema';
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
