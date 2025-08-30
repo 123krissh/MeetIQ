1d6dd1874097c7ca82101d54d9e69e9ca473a085
Mayankkk25 <10617711922_ds@vips.edu>
2025-08-30 18:54:31 +0530

    Agent filter

--- src/modules/agents/ui/components/data-pagination.tsx
+++ src/modules/agents/ui/components/data-pagination.tsx
@@ -6,13 +6,13 @@ import { Button } from "@/components/ui/button";
 interface Props {
     page: number;
     totalPages: number;
-    onpageChange:(page: number)=> void;
+    onPageChange:(page: number)=> void;
 };
 
 export const DataPagination = ( {
     page,
     totalPages,
-    onpageChange,
+    onPageChange,
 }: Props) => {
     return (
         <div className="flex items-center justify-between">
@@ -24,7 +24,7 @@ export const DataPagination = ( {
                 disabled={page ===1}
                 variant="outline"
                 size="sm"
-                onClick={() =>onpageChange(Math.max(1, page-1))}
+                onClick={() =>onPageChange(Math.max(1, page-1))}
                 >
                     Previous
                 </Button>
@@ -32,7 +32,7 @@ export const DataPagination = ( {
                 disabled={page === totalPages || totalPages ===0}
                 variant="outline"
                 size="sm"
-                onClick={()=>onpageChange(Math.min(totalPages, page+1))}
+                onClick={()=>onPageChange(Math.min(totalPages, page+1))}
                 >
                     Next
                 </Button>
