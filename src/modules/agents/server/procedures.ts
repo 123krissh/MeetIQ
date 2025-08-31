e31fecd99ec1628de63dbdfb2c6dfeb42bd332a1
Vikram <vikramranjan71122@gmail.com>
2025-08-31 22:22:01 +0530

    Meeting form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -9,7 +9,7 @@ import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@
 
 export const agentsRouter = createTRPCRouter({
 
-    update: protectedProcedure.input(agentsUpdateSchema).mutation(async ({ctx, input}) => {
+    update: protectedProcedure.input(agentsUpdateSchema).mutation(async ({ ctx, input }) => {
         const [updatedAgent] = await db.update(agents).set(input).where(
             and(
                 eq(agents.id, input.id),
@@ -17,7 +17,7 @@ export const agentsRouter = createTRPCRouter({
             )
         ).returning();
 
-        if(!updatedAgent) {
+        if (!updatedAgent) {
             throw new TRPCError({
                 code: "NOT_FOUND",
                 message: "Agent not found",
@@ -27,12 +27,12 @@ export const agentsRouter = createTRPCRouter({
     }),
     remove: protectedProcedure.input(z.object({ id: z.string() })).mutation(async ({ ctx, input }) => {
         const [removedAgent] = await db.delete(agents)
-        .where(and(
-            eq(agents.id, input.id),
-            eq(agents.userId, ctx.auth.user.id),
-        )).returning();
+            .where(and(
+                eq(agents.id, input.id),
+                eq(agents.userId, ctx.auth.user.id),
+            )).returning();
 
-        if(!removedAgent){
+        if (!removedAgent) {
             throw new TRPCError({
                 code: "NOT_FOUND",
                 message: "Agent not found",
